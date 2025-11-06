63635f6dc543cd58b4ba4cf228637f700fa20f56
dev-madhurendra <dev.madhurendra@gmail.com>
2025-11-06 23:30:46 +0530

    feat : added whatsap no

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,6 @@
+const whatsappNo = import.meta.env.VITE_WHATSAPP_NO
 export const whatsappHref = (pName: string) =>
-  `https://wa.me/919999999999?text=${encodeURIComponent(
+  `https://wa.me/${whatsappNo}/?text=${encodeURIComponent(
     `Hi! I'm interested in ${pName}. Could you share more details?`
   )}`;
 
