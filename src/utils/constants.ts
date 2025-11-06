dd5a597bf87c08652781485cc117b0ca46a531ea
dev-madhurendra <dev.madhurendra@gmail.com>
2025-11-06 23:43:56 +0530

    feat : modified text

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 const whatsappNo = import.meta.env.VITE_WHATSAPP_NO
 export const whatsappHref = (pName: string) =>
   `https://wa.me/${whatsappNo}/?text=${encodeURIComponent(
-    `Hi! I'm interested in ${pName}. Could you share more details?`
+    `**Hi! I'm interested in ${pName}.** Could you share more details?`
   )}`;
 
 export function slugify(str: string): string {
